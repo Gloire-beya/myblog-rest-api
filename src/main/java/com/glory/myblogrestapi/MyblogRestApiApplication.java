fe429eecf06b6cf91bc361beb8e3560ce63f57f5
Gloire Beya <gloweb78@gmail.com>
2022-11-05 07:33:05 +0200

    First commit

---
+++ src/main/java/com/glory/myblogrestapi/MyblogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.glory.myblogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogRestApiApplication.class, args);
+	}
+
+}
